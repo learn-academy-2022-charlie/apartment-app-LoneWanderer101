0b99f14766681b669bd5ec13a661b29474292b53
LoneWanderer101 <sgtlinares@gmail.com>
2022-07-06 11:56:41 -0700

    intial setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_11_152050) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_06_170957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "apartments", force: :cascade do |t|
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.string "manager"
+    t.string "email"
+    t.string "price"
+    t.integer "bedrooms"
+    t.integer "bathrooms"
+    t.string "pets"
+    t.text "image"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
